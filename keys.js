5436b0f68aa97717fb3d93e913f3769030ce7bdb
Max S <mschmell@uci.edu>
2017-08-31 23:35:07 -0700

    commit

---
+++ keys.js
@@ -0,0 +1,17 @@
+
+// API KEYS
+exports.twitterKeys = {
+	consumer_key: process.env.TWITTER_CONSUMER_KEY,
+	consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+	access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+	access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+}
+
+exports.spotifyKeys = {
+	consumer_key: process.env.SPOTIFY_ID,
+	consumer_secret: process.env.SPOTIFY_SECRET
+}
+
+exports.omdbKey = {
+	consumer_key: process.env.OMDB_API_KEY
+}
--- liri.js
+++ liri.js
@@ -1,21 +1,4 @@
-
-// API KEYS
-var twitterKeys = {
-	consumer_key: process.env.TWITTER_CONSUMER_KEY,
-	consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-	access_token_key: process.env.TWITTER_ACCESS_TOKEN,
-	access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-}
-
-var spotifyKeys = {
-	consumer_key: process.env.SPOTIFY_ID,
-	consumer_secret: process.env.SPOTIFY_SECRET
-}
-
-var omdbKey = {
-	consumer_key: process.env.OMDB_API_KEY
-}
-// END API KEYS
+var apiKeys = require('./keys.js');
 
 var twitter = require('twitter');
 var spotify = require('node-spotify-api');
@@ -25,3 +8,4 @@ var request = require('request');
 var command = process.argv[2];
 var title = process.argv[3];
 console.log(command, title);
+console.log(twitterKeys.consumer_key);
